3a39e9da91157cedd3e7d9a10323c292753e55b2
Cynthia Inga <candyinga67@gmail.com>
2022-02-18 16:10:14 +0200

    Add a delele & add & edit functions

--- src/Modules/controllTools.js
+++ src/Modules/controllTools.js
@@ -1,13 +1,17 @@
-import { getTask } from './data.js';
+/* eslint-disable import/no-cycle */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable import/prefer-default-export */
+
+import { getTask } from "./data.js";
 
 export function deleteTask() {
-  const clearTask = document.querySelector('.clear-task');
-  clearTask.addEventListener('click', () => {
+  const clearTask = document.querySelector(".clear-task");
+  clearTask.addEventListener("click", () => {
     if (getTask().length > 0) {
       const filterCompliteTask = getTask().filter(
-        (task) => task.completed !== true,
+        (task) => task.completed !== true
       );
-      localStorage.setItem('Task-list', JSON.stringify(filterCompliteTask));
+      localStorage.setItem("Task-list", JSON.stringify(filterCompliteTask));
       location.reload();
     }
   });
@@ -21,22 +25,22 @@ export function addTask() {
       this.completed = false;
     }
   }
-  const addTaskForm = document.querySelector('#add-task');
-  const data = JSON.parse(localStorage.getItem('Task-list')) || [];
-  addTaskForm.addEventListener('submit', () => {
-    const inputTaskValue = document.querySelector('#add-task-input').value;
-    if (inputTaskValue === '') return;
+  const addTaskForm = document.querySelector("#add-task");
+  const data = JSON.parse(localStorage.getItem("Task-list")) || [];
+  addTaskForm.addEventListener("submit", () => {
+    const inputTaskValue = document.querySelector("#add-task-input").value;
+    if (inputTaskValue === "") return;
     const newTask = new Task(inputTaskValue);
     data.push(newTask);
-    localStorage.setItem('Task-list', JSON.stringify(data));
+    localStorage.setItem("Task-list", JSON.stringify(data));
   });
   return data;
 }
 
 export function deleteOne(deleteIcon, taskId) {
-  deleteIcon.addEventListener('click', () => {
+  deleteIcon.addEventListener("click", () => {
     const filteredTask = getTask().filter((task) => task.index !== taskId);
-    localStorage.setItem('Task-list', JSON.stringify(filteredTask));
+    localStorage.setItem("Task-list", JSON.stringify(filteredTask));
     location.reload();
   });
 }
