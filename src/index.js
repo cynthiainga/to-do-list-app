0da366c5f0fff3ac4647232c9fbf6a4b4adbc21a
Cynthia Inga <candyinga67@gmail.com>
2022-02-18 16:24:30 +0200

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import "./style.css";
-import { getTask } from "./Modules/data.js";
-import { deleteTask, deleteOne } from "./Modules/controllTools.js";
+import './style.css';
+import { getTask } from './Modules/data.js';
+import { deleteTask, deleteOne } from './Modules/controllTools.js';
 
-export const taskList = document.querySelector(".task-list-container");
+export const taskList = document.querySelector('.task-list-container');
 
 function getInputValue(task) {
   return task.description;
@@ -12,14 +12,14 @@ getTask().forEach((task) => {
   taskList.innerHTML += `<li class="container task flex-center" draggable="true">
   <span class="left flex-center">
     <input id=${task.index} type="checkbox" ${
-    task.completed ? "checked" : ""
-  }  class="checkbox" />
+  task.completed ? 'checked' : ''
+}  class="checkbox" />
     <form class="edit-form">
      <input data-index-number=${task.index} value='${getInputValue(
-    task
-  )}' class="${task.completed ? "edit-task disabled" : "edit-task"}" ${
-    task.completed ? "disabled" : ""
-  }>
+  task,
+)}' class="${task.completed ? 'edit-task disabled' : 'edit-task'}" ${
+  task.completed ? 'disabled' : ''
+}>
    </form>
   </span>
     <span class="right">
@@ -29,43 +29,43 @@ getTask().forEach((task) => {
   </li>`;
 });
 
-export const task = document.querySelectorAll(".task");
-export const editTask = document.querySelectorAll(".edit-task");
-const editForm = document.querySelectorAll(".edit-form");
-const reload = document.querySelector(".reload");
+export const task = document.querySelectorAll('.task');
+export const editTask = document.querySelectorAll('.edit-task');
+const editForm = document.querySelectorAll('.edit-form');
+const reload = document.querySelector('.reload');
 
 editForm.forEach((form) => {
-  form.addEventListener("submit", (e) => {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
     editTask.forEach((taskList) => {
       getTask().forEach((task) => {
         if (taskList.dataset.indexNumber === task.index) {
           task.description = taskList.value;
-          localStorage.setItem("Task-list", JSON.stringify(getTask()));
+          localStorage.setItem('Task-list', JSON.stringify(getTask()));
         }
       });
     });
   });
 });
 
-reload.addEventListener("click", () => {
+reload.addEventListener('click', () => {
   window.location.reload();
 });
 
 task.forEach((item) => {
-  item.addEventListener("click", () => {
-    task.forEach((t) => t.classList.remove("focus"));
-    item.classList.add("focus");
+  item.addEventListener('click', () => {
+    task.forEach((t) => t.classList.remove('focus'));
+    item.classList.add('focus');
   });
 });
 
 deleteTask();
 
 task.forEach((item) => {
-  item.addEventListener("click", () => {
-    if (item.classList.contains("focus")) {
-      const deleteIcon = item.querySelector(".far");
-      const taskId = item.querySelector(".checkbox").id;
+  item.addEventListener('click', () => {
+    if (item.classList.contains('focus')) {
+      const deleteIcon = item.querySelector('.far');
+      const taskId = item.querySelector('.checkbox').id;
       deleteOne(deleteIcon, taskId);
     }
   });
